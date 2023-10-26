f93e8b53dfa3ec27b5133836654cf7dc458cc8a7
Marakuyaspb <marakuyaspb@gmail.com>
2023-10-26 11:49:22 +0300

    cleaning

--- front_things/catalog-craft/root.js
+++
@@ -1,4 +0,0 @@
-/* ROOT*/
-const ROOT_PRODUCTS = document.getElementById('goods_all');
-const ROOT_SPINNER = document.getElementById('spinner');
-const ROOT_ERROR = document.getElementById('error');
