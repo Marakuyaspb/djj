134e2cc054f3b12a4576100cd041f7789b31fcf1
Marakuyaspb <marakuyaspb@gmail.com>
2023-10-30 17:45:22 +0300

    test parsing done

--- product/models.py
+++ product/models.py
@@ -37,45 +37,74 @@ class Product(models.Model):
 		related_name='products',
 		on_delete=models.CASCADE)
 	category_ru = models.CharField(max_length=50)
-	name = models.CharField(max_length=50) # Кресло Consono
+	product_full_name = models.CharField(max_length=50) # Кресло Consono
 	product_type = models.CharField(max_length=50) # Кресло
-	# image = models.FilePathField(path="/product/static", match="foo.*", recursive=True)
+	product_img = models.CharField(max_length=150)
 	description = models.TextField(blank=True)
 	price = models.DecimalField(max_digits=10, decimal_places=2)
 	price_sale = models.DecimalField(max_digits=10, decimal_places=2)
+	fabric_type = models.CharField(max_length=50)
+	fabric_name = models.CharField(max_length=50)
+	product_fabric_about = models.CharField(max_length=700)
+	product_fabric_img = models.CharField(max_length=150)
+	carousel_item_1 = models.CharField(max_length=150)
+	carousel_item_2 = models.CharField(max_length=150)
+	carousel_item_3 = models.CharField(max_length=150)
+	carousel_item_4 = models.CharField(max_length=150)
+	carousel_item_5 = models.CharField(max_length=150)
+	carousel_item_mob_1 = models.CharField(max_length=150)
+	carousel_item_mob_2 = models.CharField(max_length=150)
+	carousel_item_mob_3 = models.CharField(max_length=150)
+	carousel_item_mob_4 = models.CharField(max_length=150)
+	carousel_item_mob_5 = models.CharField(max_length=150)
+	closeup = models.CharField(max_length=150)
+	slider_interior_1 = models.CharField(max_length=150)
+	slider_interior_2 = models.CharField(max_length=150)
+	slider_interior_3 = models.CharField(max_length=150)
+	slider_interior_4 = models.CharField(max_length=150)
+	slider_interior_1_mob = models.CharField(max_length=150)
+	slider_interior_2_mob = models.CharField(max_length=150)
+	slider_interior_3_mob = models.CharField(max_length=150)
+	slider_interior_4_mob = models.CharField(max_length=150)
+	width = models.IntegerField(blank=True, null=True)
+	depth = models.IntegerField(blank=True, null=True)
+	height = models.IntegerField(blank=True, null=True)
+	scheme = models.CharField(max_length=150)
+	features = models.CharField(max_length=350)
 	is_new = models.BooleanField(default=True)
 	available_for_delivery_2 = models.BooleanField(default=True)
 	available_for_delivery_28 = models.BooleanField(default=True)
 	available_in_showroom = models.BooleanField(default=True)
-	fabric_type = models.CharField(max_length=50)
-	fabric_name = models.CharField(max_length=50)
-	product_fabric_about = models.CharField(max_length=700)
+	created = models.DateTimeField(auto_now_add=True)
+	updated = models.DateTimeField(auto_now=True)
+	slug = models.SlugField(max_length=250)
+
+	#scheme = models.FilePathField(path=settings.FILE_PATH_FIELD_DIRECTORY)
+	# image = models.FilePathField(path="/product/static", match="foo.*", recursive=True)
+	# is_new = models.BooleanField(default=True)
+	# available_for_delivery_2 = models.BooleanField(default=True)
+	# available_for_delivery_28 = models.BooleanField(default=True)
+	# available_in_showroom = models.BooleanField(default=True)
 	# product_fabric_img = models.FilePathField(path=settings.FILE_PATH_FIELD_DIRECTORY)
 	# carousel_item = models.FilePathField(path=settings.FILE_PATH_FIELD_DIRECTORY)
 	# carousel_item_mob = models.FilePathField(path=settings.FILE_PATH_FIELD_DIRECTORY)
 	# closeup = models.FilePathField(path=settings.FILE_PATH_FIELD_DIRECTORY)
 	# slider_interior = models.FilePathField(path=settings.FILE_PATH_FIELD_DIRECTORY)
 	# slider_interior_mob = models.FilePathField(path=settings.FILE_PATH_FIELD_DIRECTORY)
-	prod_width = models.IntegerField(blank=True, null=True)
-	prod_depth = models.IntegerField(blank=True, null=True)
-	prod_height = models.IntegerField(blank=True, null=True)
-	# scheme = models.FilePathField(path=settings.FILE_PATH_FIELD_DIRECTORY)
-	created = models.DateTimeField(auto_now_add=True)
-	updated = models.DateTimeField(auto_now=True)
-	slug = models.SlugField(max_length=250)
+	
 	
 	class Meta:
-		ordering = ['name']
+		ordering = ['product_full_name']
 		indexes = [
 			models.Index(fields=['id', 'slug']),
-			models.Index(fields=['name']),
+			models.Index(fields=['product_full_name']),
 			models.Index(fields=['-created']),
 		]
 		verbose_name = 'Товар'
 		verbose_name_plural = 'Товары'
 
 	def __str__(self):
-		return self.name
+		return self.product_full_name
 		return self.price
 		return self.price_sale
 		return self.prod_width
