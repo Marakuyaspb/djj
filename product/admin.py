d09071ff869ad2a83a5ca277aac36e79473a5ee9
Marakuyaspb <marakuyaspb@gmail.com>
2023-11-07 14:48:31 +0300

    slug autogenerate now

--- product/admin.py
+++ product/admin.py
@@ -2,5 +2,12 @@ from django.contrib import admin
 from .models import Category, Collection, Product
 
 admin.site.register(Category)
+
 admin.site.register(Collection)
-admin.site.register(Product)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+	list_display = ['product_full_name', 'fabric_name', 'is_new', 'available_for_delivery_2', 'available_for_delivery_28', 'available_in_showroom', 'created', 'updated']
+	search_fields = ['collection', 'category', 'category_ru', 'product_full_name', 'fabric_name']
+	prepopulated_fields = {'slug': ('collection','category','fabric_name')}
