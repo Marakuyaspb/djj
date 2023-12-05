583e572d203ad856a32bfd4ec31b1888e51f631e
Marakuyaspb <marakuyaspb@gmail.com>
2023-12-05 21:06:55 +0300

    fix main page link

--- product/urls.py
+++ product/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, include
+from django.conf import settings
 from django.conf.urls.static import static
 from . import views
 from django.urls import reverse
@@ -31,3 +32,5 @@ urlpatterns = [
 	#sitemap
 	path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap')
 ]
+if settings.DEBUG:
+	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
