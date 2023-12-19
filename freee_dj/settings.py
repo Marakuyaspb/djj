605db383425016f4460c352c85988e773a6ce593
Marakuyaspb <marakuyaspb@gmail.com>
2023-12-19 03:53:50 +0300

    fix smth else

--- freee_dj/settings.py
+++ freee_dj/settings.py
@@ -3,6 +3,7 @@ import os, sys
 from dotenv import load_dotenv
 from django.contrib.messages import constants as messages
 
+
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
 
@@ -98,7 +99,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
