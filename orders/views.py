e8b767bbd51e4860d5b7b7a3fc7e38a63b5cf673
Marakuyaspb <marakuyaspb@gmail.com>
2023-12-29 20:34:00 +0300

    order created page looks good

--- orders/views.py
+++ orders/views.py
@@ -25,7 +25,7 @@ def order_create(request):
 			for item in cart:
 				OrderItem.objects.create(order=order, product=item['product'],	price=item['price'], quantity=item['quantity'])
 			cart.clear()
-			
+
 	# отправить письмо покупателю
 			#order_created.delay(order.id)
 
